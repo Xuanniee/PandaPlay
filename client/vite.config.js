104ff308ad24b210715740c9cd969da440edebcd
Xuannie <xuanyi1603@gmail.com>
2024-07-28 21:34:01 +0800

    modified vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build',
+    chunkSizeWarningLimit: 1000, // Increase the limit to handle large chunks
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Example of manual chunks if needed
+        },
+      },
+    },
+  },
+});
